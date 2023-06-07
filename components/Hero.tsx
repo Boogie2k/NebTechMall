ca3849c91d54618a2db7edf23c5e608b5c733382
Boogie2k <oghosabenjamin@gmail.com>
2023-06-07 08:30:12 +0100

    Modified the buttons on the hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -13,9 +13,9 @@ import { Carousel } from 'react-responsive-carousel';
       onClick={clickHandler}
       title={label}
      /*   className={`${navStyles.carouselArrow} ${navStyles.carouselArrowLeft}`}*/
-    style={{   backgroundColor: 'orange' , position:'absolute', top:'50vh', left:'5vw', zIndex:'9', width:'50px',height:'50px' }}// Customized background color 
+    style={{  color:'white', backgroundColor: 'orange' , borderRadius:'50%', position:'absolute', top:'50vh', left:'5vw', zIndex:'9', width:'50px',height:'50px' }}// Customized background color 
     >
-    <svg style={{ }} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-left" viewBox="0 0 16 16">
+    <svg style={{marginRight:'auto', marginLeft:'auto' }} xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-chevron-left" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
 </svg>
     </button>
@@ -32,8 +32,8 @@ const renderArrowNext = (clickHandler:any, hasNext:any, label:any) => {
       onClick={clickHandler}
       title={label}
  /*       className={`${navStyles.carouselArrow} ${navStyles.carouselArrowRight}`}*/
-   style={{ backgroundColor: 'orange', position:'absolute', top:'50vh', left:'85vw', width:'50px',height:'50px' }}// Customized background color 
-    > <svg style={{marginRight:'auto', marginLeft:'auto'}}  xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
+   style={{color:'white', backgroundColor: 'orange', borderRadius:'50%', position:'absolute', top:'50vh', left:'85vw', width:'50px',height:'50px' }}// Customized background color 
+    > <svg style={{marginRight:'auto', marginLeft:'auto'}}  xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-chevron-right" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
 </svg>
     </button>
